7a7ed65a2bbd70a933926d040bd048b829ceaf4c
ArsenalGear <sanya2005@mail.ru>
2019-10-19 18:51:44 +0300

    Блог

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class); //Добавятся юзеры
+        $this->call(BlogCategoriesTableSeeder::class); //Добавятся категории
+//	    $factory->define(\App\Models\BlogPost::class, function (Faker $faker); //с помощью фактори создадутся 100штук постов
+	    factory(\App\Models\BlogPost::class, 100)->create(); //хелпер factory
+	    //росто вызывает 100 раз модель BlogPost без параметров. Как оно находит фабрику BlogPostFactory? Просто по наванию прибавляя Factory? Почему такое неочевидное решение без прямой ссылки на файл фабрики?
+     
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return view('test');
+});
+
+//
+//Auth::routes(['register' => false]);
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+// /home -страница,
+	// HomeController - контроллер из папки app/Http/Controllers/HomeController.php
+	//@index - public function index() - каую вьюху отдать
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//создаем маршрут, который является ресурсом с названием rest (название URL), за этот ресурс отвечает RestTestController, restTest - имя маршрута
+Route::resource('rest', 'RestTestController')->names('restTest');
